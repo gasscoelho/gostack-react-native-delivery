605ff4aad7a7aa68c752a2f0d6682973d8f36f94
Gabriel Coelho <gaaucocoelho@gmail.com>
2020-08-16 20:47:25 -0300

    feat: implement Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -55,11 +55,38 @@ const Dashboard: React.FC = () => {
 
   async function handleNavigate(id: number): Promise<void> {
     // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
+  }
+
+  interface GetFoodsParams {
+    category_like?: number;
+    name_like?: string;
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      const params = {} as GetFoodsParams;
+
+      if (searchValue) {
+        params.name_like = searchValue;
+      }
+
+      if (selectedCategory) {
+        params.category_like = selectedCategory;
+      }
+
+      const response = await api.get<Food[]>('/foods', {
+        params,
+      });
+
+      const foodsFormatted = response.data.map(food => {
+        return {
+          ...food,
+          formattedPrice: formatValue(food.price),
+        };
+      });
+
+      setFoods(foodsFormatted);
     }
 
     loadFoods();
@@ -68,6 +95,9 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadCategories(): Promise<void> {
       // Load categories from API
+      api.get('/categories').then(response => {
+        setCategories(response.data);
+      });
     }
 
     loadCategories();
@@ -75,6 +105,11 @@ const Dashboard: React.FC = () => {
 
   function handleSelectCategory(id: number): void {
     // Select / deselect category
+    if (selectedCategory === id) {
+      setSelectedCategory(undefined);
+    } else {
+      setSelectedCategory(id);
+    }
   }
 
   return (
